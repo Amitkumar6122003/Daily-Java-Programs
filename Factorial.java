1070191da471e61f03797e8da444be43030348b5
Amit Vikas Kadam <116537539+Amitkumar6122003@users.noreply.github.com>
2025-07-09 22:23:33 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+package com.practise;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		
+		int num = sc.nextInt();
+		
+		long fact = 1;
+		for (int i=1; i<=num; i++)
+		{
+			fact *=i;
+		}
+		System.out.println("Factorial of " + num + " is: " + fact);
+	}
+
+}
